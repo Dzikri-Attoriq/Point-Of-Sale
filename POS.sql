06e3b76c0ca7b9d2e309c12ad8fbc4f17b082d59
Dzikri-Attoriq <119926410+Dzikri-Attoriq@users.noreply.github.com>
2022-12-06 17:00:44 +0800

    Add files via upload

---
+++ POS.sql
@@ -0,0 +1,480 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 06, 2022 at 09:22 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mypos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `gender` enum('L','P') NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customer_id`, `name`, `gender`, `phone`, `address`, `created`, `updated`) VALUES
+(1, 'Dzikri Attoriq Julianto', 'L', '089602054090', 'Penajam Paser Utara', '2022-11-04 14:51:36', NULL),
+(2, 'Ghedira Felicia Febyanti', 'P', '0821934984', 'Samarinda', '2022-11-04 14:51:36', '2022-11-04 09:26:58'),
+(4, 'xzxxzxz', 'P', '896020540905645', '677ytyty', '2022-11-04 16:00:23', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `p_category`
+--
+
+CREATE TABLE `p_category` (
+  `category_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `p_category`
+--
+
+INSERT INTO `p_category` (`category_id`, `name`, `created`, `updated`) VALUES
+(10, 'Minuman', '2022-11-04 17:32:50', '2022-12-05 15:21:29'),
+(11, 'Permen', '2022-11-15 13:37:52', '2022-12-05 14:18:29'),
+(12, 'Cemilan', '2022-11-15 13:38:53', NULL),
+(17, 'asdas', '2022-12-05 22:17:17', '2022-12-05 15:17:59'),
+(18, 'dasas', '2022-12-05 22:17:37', '2022-12-05 15:22:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `p_item`
+--
+
+CREATE TABLE `p_item` (
+  `item_id` int(11) NOT NULL,
+  `barcode` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `category_id` int(11) NOT NULL,
+  `unit_id` int(11) NOT NULL,
+  `price` int(11) DEFAULT NULL,
+  `stock` int(10) NOT NULL DEFAULT 0,
+  `image` varchar(100) DEFAULT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `p_item`
+--
+
+INSERT INTO `p_item` (`item_id`, `barcode`, `name`, `category_id`, `unit_id`, `price`, `stock`, `image`, `created`, `updated`) VALUES
+(1, 'A0001234', 'Golda', 10, 9, 50000, 125, NULL, '2022-11-05 11:12:49', '2022-11-05 10:26:00'),
+(3, 'A0001', 'NutriBoost', 10, 10, 80000, 109, NULL, '2022-11-05 11:15:37', '2022-11-05 10:25:53'),
+(5, 'A002', 'Teh Pucuk', 10, 9, 4000, 8, NULL, '2022-11-05 18:36:08', NULL),
+(7, 'A005', 'Tea Tea', 10, 9, 12000, 0, 'item-221106-89a9bb3951.jpg', '2022-11-06 11:31:16', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `p_unit`
+--
+
+CREATE TABLE `p_unit` (
+  `unit_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `p_unit`
+--
+
+INSERT INTO `p_unit` (`unit_id`, `name`, `created`, `updated`) VALUES
+(9, 'Botol', '2022-11-04 17:31:20', NULL),
+(10, 'Kilogram', '2022-11-04 17:32:14', NULL),
+(12, 'xxx', '2022-11-15 14:33:58', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `supplier_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `description` text DEFAULT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`supplier_id`, `name`, `phone`, `address`, `description`, `created`, `updated`) VALUES
+(3, 'Toko C', '09310923102', 'Bandung', 'Toko Manufacture', '2022-11-04 13:01:50', '2022-11-15 09:12:28'),
+(6, 'Toko Z', '09301293102', 'Kalimantan Timur', NULL, '2022-11-04 13:39:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_cart`
+--
+
+CREATE TABLE `t_cart` (
+  `cart_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `discount_item` int(11) DEFAULT NULL,
+  `total` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_sale`
+--
+
+CREATE TABLE `t_sale` (
+  `sale_id` int(11) NOT NULL,
+  `invoice` varchar(50) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `total_price` int(11) NOT NULL,
+  `discount` int(11) NOT NULL,
+  `final_price` int(11) NOT NULL,
+  `cash` int(11) NOT NULL,
+  `remaining` int(11) NOT NULL,
+  `note` text DEFAULT NULL,
+  `date` date NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `t_sale`
+--
+
+INSERT INTO `t_sale` (`sale_id`, `invoice`, `customer_id`, `total_price`, `discount`, `final_price`, `cash`, `remaining`, `note`, `date`, `user_id`, `created`) VALUES
+(99, 'MP2212010001', NULL, 104000, 0, 104000, 110000, 6000, '', '2022-12-01', 9, '2022-12-01 08:18:10'),
+(100, 'MP2212010002', NULL, 54000, 0, 54000, 54000, 0, '', '2022-12-01', 9, '2022-12-01 08:18:59'),
+(101, 'MP2212010003', NULL, 54000, 0, 54000, 54000, 0, '', '2022-12-01', 9, '2022-12-01 08:19:22'),
+(103, 'MP2212010004', NULL, 54000, 0, 54000, 54000, 0, '', '2022-12-01', 9, '2022-12-01 08:21:51'),
+(104, 'MP2212020001', NULL, 80000, 0, 80000, 80000, 0, '', '2022-12-02', 9, '2022-12-02 15:47:47'),
+(105, 'MP2212020002', NULL, 80000, 0, 80000, 80000, 0, '', '2022-12-02', 9, '2022-12-02 15:49:02'),
+(106, 'MP2212020003', NULL, 80000, 0, 80000, 80000, 0, '', '2022-12-02', 9, '2022-12-02 15:50:16'),
+(107, 'MP2212040001', NULL, 1170000, 0, 1170000, 1170000, 0, '', '2022-12-04', 9, '2022-12-04 15:23:44');
+
+--
+-- Triggers `t_sale`
+--
+DELIMITER $$
+CREATE TRIGGER `del_detail` AFTER DELETE ON `t_sale` FOR EACH ROW BEGIN
+	DELETE FROM t_sale_detail
+    WHERE sale_id = OLD.sale_id;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_sale_detail`
+--
+
+CREATE TABLE `t_sale_detail` (
+  `detail_id` int(11) NOT NULL,
+  `sale_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `qty` int(20) NOT NULL,
+  `discount_item` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `t_sale_detail`
+--
+
+INSERT INTO `t_sale_detail` (`detail_id`, `sale_id`, `item_id`, `price`, `qty`, `discount_item`, `total`) VALUES
+(53, 99, 1, 50000, 2, 0, 100000),
+(54, 99, 5, 4000, 1, 0, 4000),
+(55, 100, 1, 50000, 1, 0, 50000),
+(56, 100, 5, 4000, 1, 0, 4000),
+(57, 101, 1, 50000, 1, 0, 50000),
+(58, 101, 5, 4000, 1, 0, 4000),
+(59, 103, 1, 50000, 1, 0, 50000),
+(60, 103, 5, 4000, 1, 0, 4000),
+(61, 107, 3, 80000, 14, 0, 1120000),
+(62, 107, 1, 50000, 1, 0, 50000);
+
+--
+-- Triggers `t_sale_detail`
+--
+DELIMITER $$
+CREATE TRIGGER `stock_min` AFTER INSERT ON `t_sale_detail` FOR EACH ROW BEGIN
+	UPDATE p_item SET stock = stock - NEW.qty
+    WHERE item_id = NEW.item_id;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `stock_return` AFTER DELETE ON `t_sale_detail` FOR EACH ROW BEGIN
+	UPDATE p_item SET stock = stock + OLD.qty
+    WHERE item_id = OLD.item_id;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_stock`
+--
+
+CREATE TABLE `t_stock` (
+  `stock_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `type` enum('in','out') NOT NULL,
+  `detail` varchar(200) NOT NULL,
+  `supplier_id` int(11) DEFAULT NULL,
+  `qty` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `t_stock`
+--
+
+INSERT INTO `t_stock` (`stock_id`, `item_id`, `type`, `detail`, `supplier_id`, `qty`, `date`, `created`, `user_id`) VALUES
+(3, 1, 'in', 'Tambahan', 6, 123, '2022-11-08', '2022-11-08 13:26:12', 9),
+(4, 1, 'in', 'Tambahan', 3, 13, '2022-11-08', '2022-11-08 13:28:30', 9),
+(7, 5, 'in', 'Tambahan', 6, 12, '2022-11-09', '2022-11-09 09:16:15', 9),
+(13, 3, 'in', 'Tambahan', 6, 133, '2022-11-15', '2022-11-15 15:33:15', 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `level` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password`, `name`, `address`, `image`, `level`) VALUES
+(9, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Dzikri Attoriq Julianto', 'Penajam Paser Utara', 'my6.jpg', 1),
+(10, 'kasir1', '874c0ac75f323057fe3b7fb3f5a8a41df2b94b1d', 'Humanxiety', 'Samarinda', 'default.jpg', 2),
+(15, 'zzzzz', '8cb2237d0679ca88db6464eac60da96345513964', 'zzzz', 'weqweqw', 'default.jpg', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `p_category`
+--
+ALTER TABLE `p_category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `p_item`
+--
+ALTER TABLE `p_item`
+  ADD PRIMARY KEY (`item_id`),
+  ADD UNIQUE KEY `barcode` (`barcode`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `unit_id` (`unit_id`);
+
+--
+-- Indexes for table `p_unit`
+--
+ALTER TABLE `p_unit`
+  ADD PRIMARY KEY (`unit_id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`supplier_id`);
+
+--
+-- Indexes for table `t_cart`
+--
+ALTER TABLE `t_cart`
+  ADD PRIMARY KEY (`cart_id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `t_sale`
+--
+ALTER TABLE `t_sale`
+  ADD PRIMARY KEY (`sale_id`);
+
+--
+-- Indexes for table `t_sale_detail`
+--
+ALTER TABLE `t_sale_detail`
+  ADD PRIMARY KEY (`detail_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `t_stock`
+--
+ALTER TABLE `t_stock`
+  ADD PRIMARY KEY (`stock_id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `supplier_id` (`supplier_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `p_category`
+--
+ALTER TABLE `p_category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `p_item`
+--
+ALTER TABLE `p_item`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `p_unit`
+--
+ALTER TABLE `p_unit`
+  MODIFY `unit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `supplier_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `t_sale`
+--
+ALTER TABLE `t_sale`
+  MODIFY `sale_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+
+--
+-- AUTO_INCREMENT for table `t_sale_detail`
+--
+ALTER TABLE `t_sale_detail`
+  MODIFY `detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
+--
+-- AUTO_INCREMENT for table `t_stock`
+--
+ALTER TABLE `t_stock`
+  MODIFY `stock_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `p_item`
+--
+ALTER TABLE `p_item`
+  ADD CONSTRAINT `p_item_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `p_category` (`category_id`),
+  ADD CONSTRAINT `p_item_ibfk_2` FOREIGN KEY (`unit_id`) REFERENCES `p_unit` (`unit_id`);
+
+--
+-- Constraints for table `t_cart`
+--
+ALTER TABLE `t_cart`
+  ADD CONSTRAINT `t_cart_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `p_item` (`item_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `t_cart_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `t_sale_detail`
+--
+ALTER TABLE `t_sale_detail`
+  ADD CONSTRAINT `t_sale_detail_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `p_item` (`item_id`);
+
+--
+-- Constraints for table `t_stock`
+--
+ALTER TABLE `t_stock`
+  ADD CONSTRAINT `t_stock_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `p_item` (`item_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `t_stock_ibfk_2` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`supplier_id`),
+  ADD CONSTRAINT `t_stock_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
